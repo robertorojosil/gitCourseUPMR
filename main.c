f8df12c74f9a16b483ddccda62a35f4b6085270b
Roberto <robertoorusco@gmail.com>
2023-10-03 22:16:47 +0200

    add some c code as example

---
+++ main.c
@@ -0,0 +1,44 @@
+/* 
+ * Front Signals v1.1
+ * UPMR 2022
+ */
+#define _SUPPRESS_PLIB_WARNING
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p32xxxx.h>
+#include <plib.h>
+#include "app/main_config.h"
+#include "app/output_comm.h"
+
+int main() {
+    // System Initialization
+    initialize_system();
+
+    while(true){
+        //Read analog values
+        can_comm_front_sensors_t frontSensors;
+
+        frontSensors.steering = 0;
+        frontSensors.brake = 0;
+        frontSensors.throttle_1 = 0;
+        frontSensors.throttle_2 = 0;
+        frontSensors.brake_pressure_1 = 0;
+        frontSensors.brake_pressure_2 = 0;
+        frontSensors.brake_pressure_3 = 0;
+
+        frontSensors.throttle_1 = analog_Throttle(4);
+        frontSensors.throttle_2 = analog_Throttle(5);
+        frontSensors.brake = analog_Brake();
+        frontSensors.steering = analog_steering();
+        
+        frontSensors.brake_pressure_1 = analog_BrakePressure(10);
+        frontSensors.brake_pressure_2 = analog_BrakePressure(8);
+        frontSensors.brake_pressure_3 = analog_BrakePressure(9);
+        
+        can_comm_front(&frontSensors);
+
+        time_delay(1,10);
+    }
+    return (EXIT_SUCCESS);
+}
